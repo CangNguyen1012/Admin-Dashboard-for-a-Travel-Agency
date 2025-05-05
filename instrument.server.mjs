430a991994214575af228e68ccf56933c230964f
CangNguyen <brunomessi933@gmail.com>
2025-05-05 11:32:59 +0700

    Entreprise Error Tracking & Monitoring

---
+++ instrument.server.mjs
@@ -0,0 +1,14 @@
+import * as Sentry from "@sentry/react-router";
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
+
+Sentry.init({
+  dsn: "https://69d17dd0a39eb78751204de26c63d314@o4509234359828480.ingest.us.sentry.io/4509234404655104",
+
+  // Adds request headers and IP for users, for more info visit:
+  // https://docs.sentry.io/platforms/javascript/guides/react-router/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+
+  integrations: [nodeProfilingIntegration()],
+  tracesSampleRate: 1.0, // Capture 100% of the transactions
+  profilesSampleRate: 1.0, // profile every transaction
+});
--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,31 @@ import { reactRouter } from "@react-router/dev/vite"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "vite"
 import tsconfigPaths from "vite-tsconfig-paths"
+import {
+    sentryReactRouter,
+    type SentryReactRouterBuildOptions,
+} from "@sentry/react-router"
 
-export default defineConfig({
-    plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-    ssr: {
-        noExternal: [/@syncfusion/],
-    },
+const sentryConfig: SentryReactRouterBuildOptions = {
+    org: "personal-k99",
+    project: "travel-agency",
+    // An auth token is required for uploading source maps.
+    authToken:
+        "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",
+    // ...
+}
+
+export default defineConfig((config) => {
+    return {
+        plugins: [
+            reactRouter(),
+            sentryReactRouter(sentryConfig, config),
+            tailwindcss(),
+            tsconfigPaths(),
+        ],
+        sentryConfig, // Also pass the config here!
+        ssr: {
+            noExternal: [/@syncfusion/],
+        },
+    }
 })
